d3a3a6746ab25362504fdd4d21ab419a36c56892
Matt Montgomery <mmontgomery@deseretdigital.com>
2015-08-21 01:15:58 -0600

    Added very quick redis caching

--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,7 @@
 var express = require('express');
+var cache = require('express-redis-cache')({
+    expire: 60
+});
 var router = express.Router();
 var request = require('request');
 var bgg = require('../src/api/bgg');
@@ -11,17 +14,22 @@ router.get('/', function(req, res) {
     });
 })
 
-router.get('/collection/:user', function(req, res) {
-    var collection = bgg.getCollection(req.params.user);
-    collection.then(function(collection) {
-        collection = (collection && collection.items && _.isArray(collection.items.item)) ? collection.items.item : collection;
-        res.set({
-            'Access-Control-Allow-Origin': '*'
-        }).json({
-            requestedUser: req.params.user,
-            collection: collection
+router.get(
+    '/collection/:user',
+    function(req, res, next) {
+        res.set({'Access-Control-Allow-Origin':'*'});
+        next();
+    },
+    cache.route(),
+    function(req, res) {
+        var collection = bgg.getCollection(req.params.user);
+        collection.then(function(collection) {
+            collection = (collection && collection.items && _.isArray(collection.items.item)) ? collection.items.item : collection;
+            res.json({
+                requestedUser: req.params.user,
+                collection: collection
+            });
         });
     });
-});
 
 module.exports = router;
