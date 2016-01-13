0d1b0ee95e6213f8f0bac7e4d93493b625a48050
Matt Montgomery <mmontgomery@deseretdigital.com>
2016-01-13 03:21:14 -0700

    .

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,8 @@
 var express = require('express');
-/*var cache = require('express-redis-cache')({
+var cache = require('express-redis-cache')({
     expire: 5,
     host: process.env.REDIS_URL
-});*/
+});
 var router = express.Router();
 var request = require('request');
 var bgg = require('../src/api/bgg');
@@ -21,7 +21,7 @@ router.get(
         res.set({'Access-Control-Allow-Origin':'*'});
         next();
     },
-    /*cache.route(),*/
+    cache.route(),
     function(req, res) {
         var collection = bgg.getCollection(req.params.user);
         collection.then(function(collection) {
